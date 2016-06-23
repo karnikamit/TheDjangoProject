a1018d0d165eca8b7fa582b624ada5ca0487382d
kanrikamit <karnikmait46@gmail.com>
2016-06-23 07:23:04 +0530

    Rendering template:start, add other methods

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
 __author__ = 'karnikamit'
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello, world! you are at the polls index!')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    questions = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html', context=questions)
+
+
+def detail(request, question_id):
+    return HttpResponse("you're reading question ID: %d " % int(question_id))
+
+
+def results(request, question_id):
+    return HttpResponse("you're reading results for %d" % int(question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse("you're voting to question id: %d" % int(question_id))
+
